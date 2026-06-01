c1ef5a81c3de42176e3cc2f53c4b1aa5dc2819af
Sanjana H S <sanjugangatkar068@gmail.com>
2026-06-01 23:43:28 +0530

    Update .py

--- .py
+++ .py
@@ -3,9 +3,14 @@ from flask import Flask, render_template_string, request, redirect, url_for, ses
 app = Flask(__name__)
 app.secret_key = "attendance_secret_key"
 
+# Dummy teacher data
 teachers = {
     "teacher1": "1234",
     "admin": "admin123"
 }
+
+# Login Page HTML
 login_page = """
 <!DOCTYPE html>
+<html>
+<head>
